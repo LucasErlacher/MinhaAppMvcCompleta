b0ab0c2ad23c260c5ba22bab9859d9211c8b4add
Lucas Erlacher Rodrigues <lucaserlacher@hotmail.com>
2020-01-20 08:26:43 -0300

    ajuste de frontend

---
+++ src/DevIO.App/Extensions/RazorExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.Razor;
+using System;
+
+namespace DevIO.App.Extensions
+{
+    public static class RazorExtensions
+    {
+        public static string FormataDocumento(this RazorPage _, int tipoDoc, string documento)
+        {
+            return tipoDoc == 1 ?
+                  Convert.ToUInt64(documento).ToString(@"000\.000\.000\-00") :
+                  Convert.ToUInt64(documento).ToString(@"00\.000\.000\/0000\-00");
+        }
+    }
+}
