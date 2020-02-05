d41f815b898e4e281fbb958f3dc2f198c9eb21cb
Lucas Erlacher Rodrigues <lucas.rodrigo@aevo.com.br>
2020-02-05 17:01:25 -0300

    organização da classe startup utilizando extension method

---
+++ src/DevIO.App/Configurations/MvcConfig.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace DevIO.App.Configurations
+{
+    public static class MvcConfig
+    {
+        public static IServiceCollection AddMvcConfiguration(this IServiceCollection services)
+        {
+            services.AddMvc(setup =>
+            {
+                var messageProvider = setup.ModelBindingMessageProvider;
+
+                messageProvider.SetAttemptedValueIsInvalidAccessor((x, y) => "O valor preenchido é inválido para este campo.");
+                messageProvider.SetMissingBindRequiredValueAccessor(x => "Este campo precisa ser preenchido.");
+                messageProvider.SetMissingKeyOrValueAccessor(() => "Este campo precisa ser preenchido.");
+                messageProvider.SetMissingRequestBodyRequiredValueAccessor(() => "É necessário que o body na requisição não esteja vazio.");
+                messageProvider.SetNonPropertyAttemptedValueIsInvalidAccessor(x => "O valor preenchido é inválido para este campo.");
+                messageProvider.SetNonPropertyUnknownValueIsInvalidAccessor(() => "O valor preenchido é inválido para este campo.");
+                messageProvider.SetNonPropertyValueMustBeANumberAccessor(() => "O campo deve ser numérico");
+                messageProvider.SetUnknownValueIsInvalidAccessor(x => "O valor preenchido é inválido para este campo.");
+                messageProvider.SetValueIsInvalidAccessor(x => "O valor preenchido é inválido para este campo.");
+                messageProvider.SetValueMustBeANumberAccessor(x => "O campo deve ser numérico.");
+                messageProvider.SetValueMustNotBeNullAccessor(x => "Este campo precisa ser preenchido.");
+            });
+
+            return services;
+        }
+    }
+}
--- src/DevIO.App/Startup.cs
+++ src/DevIO.App/Startup.cs
@@ -1,77 +1,43 @@
 using AutoMapper;
-using DevIO.App.Data;
-using DevIO.App.Extensions;
-using DevIO.Business.Interfaces;
+using DevIO.App.Configurations;
 using DevIO.Data.Context;
-using DevIO.Data.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Localization;
-using Microsoft.AspNetCore.Mvc.DataAnnotations;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System.Collections.Generic;
-using System.Globalization;
 
 namespace DevIO.App
 {
     public class Startup
     {
         public Startup(IConfiguration configuration)
-        {            
-            Configuration = configuration;     
+        {
+            Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
-        {            
-            services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(
-                    Configuration.GetConnectionString("DefaultConnection")));
-
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-                .AddEntityFrameworkStores<ApplicationDbContext>();
+        {
+            services.AddIdentityConfiguration(Configuration);
 
             services.AddDbContext<MeuDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
 
             services.AddControllersWithViews();
+
             services.AddRazorPages();
 
             services.AddAutoMapper(typeof(Startup));
 
-            services.AddMvc(setup =>
-            {
-                var messageProvider = setup.ModelBindingMessageProvider;
+            services.AddMvcConfiguration();
 
-                messageProvider.SetAttemptedValueIsInvalidAccessor((x, y) => "O valor preenchido é inválido para este campo.");
-                messageProvider.SetMissingBindRequiredValueAccessor(x => "Este campo precisa ser preenchido.");
-                messageProvider.SetMissingKeyOrValueAccessor(() => "Este campo precisa ser preenchido.");
-                messageProvider.SetMissingRequestBodyRequiredValueAccessor(() => "É necessário que o body na requisição não esteja vazio.");
-                messageProvider.SetNonPropertyAttemptedValueIsInvalidAccessor(x => "O valor preenchido é inválido para este campo.");
-                messageProvider.SetNonPropertyUnknownValueIsInvalidAccessor(() => "O valor preenchido é inválido para este campo.");
-                messageProvider.SetNonPropertyValueMustBeANumberAccessor(() => "O campo deve ser numérico");
-                messageProvider.SetUnknownValueIsInvalidAccessor(x => "O valor preenchido é inválido para este campo.");
-                messageProvider.SetValueIsInvalidAccessor(x => "O valor preenchido é inválido para este campo.");
-                messageProvider.SetValueMustBeANumberAccessor(x => "O campo deve ser numérico.");
-                messageProvider.SetValueMustNotBeNullAccessor(x => "Este campo precisa ser preenchido.");
-            });
-
-            services.AddScoped<MeuDbContext>();
-            services.AddScoped<IProdutoRepository, ProdutoRepository>();
-            services.AddScoped<IFornecedorRepository, FornecedorRepository>();
-            services.AddScoped<IEnderecoRepository, EnderecoRepository>();
-
-            services.AddSingleton<IValidationAttributeAdapterProvider, MoedaValidationAttruibuteAdapterProvider>();
+            services.ResolveDependencies();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -84,23 +50,13 @@ namespace DevIO.App
                 app.UseExceptionHandler("/Home/Error");
                 app.UseHsts();
             }
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-
             app.UseRouting();
-
             app.UseAuthentication();
             app.UseAuthorization();
-
-            CultureInfo cultureInfo = new CultureInfo("pt-BR");
-            var localizationOptions = new RequestLocalizationOptions
-            {
-                DefaultRequestCulture = new RequestCulture(cultureInfo),
-                SupportedCultures = new List<CultureInfo> { cultureInfo },
-                SupportedUICultures = new List<CultureInfo> { cultureInfo }
-            };
-
-            app.UseRequestLocalization(localizationOptions);
+            app.UseGlobalizationConfig();
 
             app.UseEndpoints(endpoints =>
             {
